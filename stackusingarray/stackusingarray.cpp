844b501f6f8efeadc65aa5b15baaa5699df1fa12
adwitafernanda <adwitafernanda4@gmail.com>
2023-06-15 10:39:22 +0700

    membuat class program

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -3,8 +3,48 @@
 
 using namespace std;
 
-class program {
+class Program {
 private:
 	string stack_array[5];
 	int top;
+
+public:
+	Program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "Stack Full" << endl;
+			return "";
+		}
+
+		top++;
+		stack_array[top] = element;
+
+		cout << endl;
+		cout << element << " ditambahkan(pushed). " << endl;
+
+		return element;
+	}
+
+	void pop() {
+		if (empty()) {
+			cout << "\nStack is empty. Cannot pop. " << endl;
+			return;
+		}
+
+		cout << "\nThe popped element is: " << stack_array[top] << endl;
+		top--;
+	}
+
+	bool empy() {
+		return (top == -1);
+	}
+
+	void display() {
+		if (empty()) {
+
+		}
+	}
 };
